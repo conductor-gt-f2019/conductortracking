0ad84893fd0ac76c38e7c576616b0e945c20f484
Phong Tran <s2@puyonexus.com>
2019-12-04 18:20:04 -0500

    help

--- accumulator.py
+++ accumulator.py
@@ -28,7 +28,7 @@ def create_accumulator_matrix(segment: pd.DataFrame, hand: str, m_size=(500, 500
     # Remove points that are precisely at zero
     seg = segment[(segment.loc[:, hand_x] != 0).values * (segment.loc[:, hand_y] != 0).values].copy()
 
-    # Normalize segment data into the range of [0, 500]
+    # Normalize segment data into the range of [0, 500 - 100]
     y_min = seg.loc[:, hand_y].min()
     x_min = seg.loc[:, hand_x].min()
     seg.loc[:, hand_y] = seg[hand_y].apply(lambda v: v - y_min)
@@ -36,23 +36,26 @@ def create_accumulator_matrix(segment: pd.DataFrame, hand: str, m_size=(500, 500
 
     y_max = seg.loc[:, hand_y].max()
     x_max = seg.loc[:, hand_x].max()
-    seg.loc[:, hand_y] = seg[hand_y].apply(lambda v: v / y_max * (m_size[0] - 1))
-    seg.loc[:, hand_x] = seg[hand_x].apply(lambda v: v / x_max * (m_size[1] - 1))
+    seg.loc[:, hand_y] = seg[hand_y].apply(lambda v: v / y_max * (m_size[0] - 100 - 1))
+    seg.loc[:, hand_x] = seg[hand_x].apply(lambda v: v / x_max * (m_size[1] - 100 - 1))
 
     # Cast to int so points can be used ast array indices
     int_segment = seg.astype('int32')
 
     # Create accumulator matrix
     acc = np.zeros(m_size)
-    acc[int_segment.loc[:, hand_y], int_segment.loc[:, hand_x]] = 1
+    acc[int_segment.loc[:, hand_y] + 50, int_segment.loc[:, hand_x] + 50] = 1
 
     return acc
 
-def save_image(acc: np.ndarray, filepath: str):
+def save_image(acc: np.ndarray, filepath: str, reverse: False):
     three_channel = np.array([acc, acc, acc])
     three_channel = three_channel.transpose(1, 2, 0)
     three_channel = three_channel * 255
-    cv2.imwrite(filepath, three_channel)
+    if reverse == False:
+        cv2.imwrite(filepath, three_channel)
+    else:
+        cv2.imwrite(filepath, cv2.flip(three_channel, 1)) # Save a horizontally flipped image too.
 
 def saveTrainAndVal(filepath:str, filename:str, hand:str, timesignature: str):
     # filepath = "pose_data/data_both_hands_2-2_g.csv"
@@ -74,13 +77,19 @@ def saveTrainAndVal(filepath:str, filename:str, hand:str, timesignature: str):
 
     for j in range(len(train_accs)):
         filepath = "data/train/" + timesignature + "/" + filename[:-4] + "_" + str(j) + ".png"
-        save_image(train_accs[j], filepath)
+        r_filepath = "data/train/" + timesignature + "/" + filename[:-4] + "_r" + str(j) + ".png"
+        save_image(train_accs[j], filepath, False)
         print("Saved: ", filepath)
+        save_image(train_accs[j], r_filepath, True)
+        print("Saved: ", r_filepath)
 
     for j in range(len(val_accs)):
         filepath = "data/val/" + timesignature + "/" + filename[:-4] + "_" + str(j + ind75) + ".png"
-        save_image(val_accs[j], filepath)
+        r_filepath = "data/val/" + timesignature + "/" + filename[:-4] + "_r" + str(j + ind75) + ".png"
+        save_image(val_accs[j], filepath, False)
         print("Saved: ", filepath)
+        save_image(val_accs[j], r_filepath, True)
+        print("Saved: ", r_filepath)
 
 # %%
 if __name__ == "__main__":    
@@ -88,9 +97,10 @@ if __name__ == "__main__":
     saveTrainAndVal("pose_data/data_both_hands_2-2_g.csv", "data_both_hands_2-2_g.csv", "left", "2_2")
     # saveTrainAndVal("pose_data/data_left_hand_3-4_g.csv", "data_left_hand_3-4_g.csv", "left", "3_4")
     # saveTrainAndVal("pose_data/data_right_hand_3-4_g.csv", "data_right_hand_3-4_g.csv", "right", "3_4")
-    saveTrainAndVal("pose_data/data_left_hand_p1_4-4_g.csv", "data_left_hand_p1_4-4_g.csv", "left", "4_4")
-    saveTrainAndVal("pose_data/data_left_hand_p2_4-4_g.csv", "data_left_hand_p2_4-4_g.csv", "left", "4_4")
-    saveTrainAndVal("pose_data/data_right_hand_4-4_g.csv", "data_right_hand_4-4_g.csv", "right", "4_4")
+    # saveTrainAndVal("pose_data/data_left_hand_p1_4-4_g.csv", "data_left_hand_p1_4-4_g.csv", "left", "4_4")
+    # saveTrainAndVal("pose_data/data_left_hand_p2_4-4_g.csv", "data_left_hand_p2_4-4_g.csv", "left", "4_4")
+    # saveTrainAndVal("pose_data/data_right_hand_4-4_g.csv", "data_right_hand_4-4_g.csv", "right", "4_4")
+    saveTrainAndVal("pose_data/data_right_hand_pt_4-4.csv", "data_right_hand_pt_4-4.csv", "right", "4_4")
     saveTrainAndVal("pose_data/data_ragh_left_p1_3-4_g.csv", "data_ragh_left_p1_3-4_g.csv", "left", "3_4")
     saveTrainAndVal("pose_data/data_ragh_right_3-4_g.csv", "data_ragh_right_3-4_g.csv", "right", "3_4")
     saveTrainAndVal("pose_data/data_ragh_right_p2_3-4_g.csv", "data_ragh_right_p2_3-4_g.csv", "right", "3_4")
--- main.py
+++ main.py
@@ -59,7 +59,6 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
 
-
         # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
             if phase == 'train':
@@ -141,9 +140,10 @@ if __name__ == "__main__":
     optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
 
     # Decay LR by a factor of 0.1 every 7 epochs
-    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+    exp_lr_scheduler = lr_scheduler.StepLR(
+        optimizer_ft, step_size=7, gamma=0.1)
 
     model_ft, hist = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                       num_epochs=25)
+                                 num_epochs=25)
 
-    torch.save(model_ft.state_dict(), 'ct_model.pt')
+    torch.save(model_ft.state_dict(), 'ct_model4.pt')
